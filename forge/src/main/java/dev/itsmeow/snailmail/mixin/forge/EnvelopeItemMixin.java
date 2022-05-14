d5e47d7d4d0140f18bef0b08889a99877afaed9d
itsmeow <itsmeow@itsmeow.dev>
2022-05-14 18:04:09 -0400

    Seperate envelope mixin

--- forge/src/main/java/dev/itsmeow/snailmail/mixin/forge/EnvelopeItemMixin.java
+++ forge/src/main/java/dev/itsmeow/snailmail/mixin/forge/EnvelopeItemMixin.java
@@ -1,18 +1,11 @@
 package dev.itsmeow.snailmail.mixin.forge;
 
-import dev.itsmeow.snailmail.init.ModItems;
 import dev.itsmeow.snailmail.item.EnvelopeItem;
-import net.minecraft.core.Direction;
+import dev.itsmeow.snailmail.util.forge.EnvelopeCapabilityProvider;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.capabilities.ICapabilitySerializable;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,45 +26,4 @@ public abstract class EnvelopeItemMixin extends Item {
         return new EnvelopeCapabilityProvider(stack, nbt, isOpen);
     }
 
-    public static class EnvelopeCapabilityProvider implements ICapabilitySerializable<CompoundTag> {
-
-        private ItemStackHandler handler;
-        public final LazyOptional<ItemStackHandler> handlerOptional;
-        private ItemStack stack;
-
-        public EnvelopeCapabilityProvider(ItemStack stack, CompoundTag compound, boolean isOpen) {
-            this.handler = new ItemStackHandler(isOpen ? 28 : 27) {
-                @Override
-                public boolean isItemValid(int slot, ItemStack stack) {
-                    if(slot == 27) {
-                        return stack.getItem() == ModItems.STAMP.get();
-                    }
-                    return !stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).isPresent() && stack.getItem() != Items.SHULKER_BOX;
-                }
-            };
-            this.handlerOptional = LazyOptional.of(() -> handler);
-            this.stack = stack;
-        }
-
-        @Override
-        public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction dir) {
-            if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && !stack.isEmpty()) {
-                return handlerOptional.cast();
-            }
-            return LazyOptional.empty();
-        }
-
-        @Override
-        public CompoundTag serializeNBT() {
-            return handler.serializeNBT();
-        }
-
-        @Override
-        public void deserializeNBT(CompoundTag nbt) {
-            if(nbt != null) {
-                handler.deserializeNBT(nbt);
-            }
-        }
-
-    }
 }
